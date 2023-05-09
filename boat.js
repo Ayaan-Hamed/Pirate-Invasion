0ed9078bb337ce4fec755ea245871af264661e4b
Ayaan-Hamed <119427820+Ayaan-Hamed@users.noreply.github.com>
2023-05-09 17:36:10 +0300

    Update boat.js

--- boat.js
+++ boat.js
@@ -1,15 +1,24 @@
 class Boat {
-    constructor (x,y,w,h,p) {
-        this.body = Matter.Bodies.rectangle (x,y,w,h, {friction : 5, density : 1, restitution : 0.8})
-        Matter.World.add (world,this.body)
-        this.w = w
-        this.h = h
-        this.p = p
+    constructor(x, y, w, h, p) {
+      this.h = h
+      var options = {
+        friction: 1, density: 1, restitution: 0.8
+      };
+      this.w = w;
+      this.body = Bodies.rectangle(x, y, this.w, this.h, options);
+      this.image = loadImage("./assets/boat.png");
+      World.add(world, this.body);
+      this.p = p
     }
-    display () {
-push ()
-translate (this.body.position.x, this.body.position.y)
-image (boatimg, 0, 0, this.w, this.h) 
-pop ()
+  
+  
+    display() 
+    {
+      push ()
+      var pos = this.body.position;
+      translate (pos.x, pos.y)
+      image(this.image, 0, this.p, this.w, this.h);
+      pop ()
     }
-}
+  }
+  
