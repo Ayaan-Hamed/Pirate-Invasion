c2db9fd49eb9832d4afd578d4bba2966eded183f
Ayaan-Hamed <119427820+Ayaan-Hamed@users.noreply.github.com>
2023-05-23 17:28:39 +0300

    Update CannonBall.js

--- js/CannonBall.js
+++ js/CannonBall.js
@@ -8,21 +8,27 @@ class CannonBall {
     this.image = loadImage("./assets/cannonball.png");
     World.add(world, this.body);
     this.path = []
+    this.flag = false
+    this.ani = [this.image]
+    this.speed = 0.05
   }
 
 
   display() 
   {
-    var pos = this.body.position;
     push ()
-    image(this.image, pos.x, pos.y, this.r, this.r);
+    var pos = this.body.position;
+    var index = floor (this.speed% this.ani.length)
+    image(this.ani [index], pos.x, pos.y, this.r, this.r);
     pop ()
-    if (this.body.velocity.x > 0 && this.body.position.x > 240) {
+
+    if (this.body.velocity.x > 0 && this.body.position.x > 220 && ! this.flag) {
       this.path.push ([this.body.position.x, this.body.position.y])
     }
     for (var i = 0 ; i < this.path.length ; i ++) {
-      image(this.image, this.path [i] [0], this.path [i] [1], 5, 5); 
+      image (this.image, this.path [i] [0], this.path [i] [1], 5, 5)
     }
+
   }
   shoot () {
 var a = cannon.angle - 30
@@ -31,5 +37,17 @@ var v = p5.Vector.fromAngle (a)
 v.mult (0.5)
 Matter.Body.setStatic (this.body, false)
 Matter.Body.setVelocity (this.body, {x:v.x*(180/3.14),y:v.y*(180/3.14)})
+  }
+  remove (i) {
+    console.log (42, cbs, i)
+    this.flag = true
+    Matter.Body.setVelocity (this.body, {x: 0, y:0})
+    this.ani = wa
+    this.path = []
+    this.r = 60
+    setTimeout (()=>{ Matter.World.remove (world, cbs [i].body)
+       delete cbs [i] ;}, 1000)
+
+    
   }
 }
